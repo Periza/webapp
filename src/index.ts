ae472f7a91fdba38c17338f83f918a6413c51d9b
Marko Perica <marko.perica2@gmail.com>
2025-11-02 22:41:27 +0100

    jsx

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import { LocalDataSource } from "./localdatasource";
+import { DomDisplay } from "./domDisplay";
+import "bootstrap/dist/css/bootstrap.css";
 
-async function displayData(): Promise<string> {
-    let ds = new LocalDataSource();
-    let allProducts = await ds.getProducts("name");
-    let categories = await ds.getCategories();
-    let chessProducts = await ds.getProducts("name", "Chess");
+let ds = new LocalDataSource();
 
-    let result = "";
+async function displayData(): Promise<HTMLElement> {
+    let display = new DomDisplay();
 
-    allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}\n`);
-    categories.forEach(c => result += (`Category: ${c}\n`));
-    chessProducts.forEach(p => ds.order.addProduct(p, 1));
-    result += `Order total: ${ds.order.total.toFixed(2)}`;
-    return result;
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+
+    return display.getContent();
 }
 
-displayData().then(res => console.log(res));
+document.onreadystatechange = () => {
+    if(document.readyState === "complete") {
+        displayData().then(elem => {
+            let rootElement = document.getElementById("app");
+            rootElement.innerHTML = "";
+            rootElement.appendChild(elem);
+        })
+    }
+}
